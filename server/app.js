67c0c0d2a640150b80f22f1acfac638b1c3e5abb
youssif hany <youssifhany75@gmail.com>
2021-11-19 21:07:30 +0200

    add http-error

--- server/app.js
+++ server/app.js
@@ -2,7 +2,20 @@ import express from "express";
 import bodyParser from "body-parser";
 
 import placesRouter from "./routes/places-routes.js";
+import usersRouter from "./routes/users-routes.js";
 
 const app = express();
-app.use(placesRouter);
+
+app.use("/api/places", placesRouter);
+app.use("/api/users", usersRouter);
+
+app.use((error, req, res, next) => {
+  if (res.headerSent) {
+    return next(error);
+  }
+
+  res.status(error.code || 500);
+  res.json({ message: error.message || "An unknown error occurred!" });
+});
+
 app.listen(5000);
--- server/routes/places-routes.js
+++ server/routes/places-routes.js
@@ -1,10 +1,55 @@
 import { Router } from "express";
 
+import HttpError from "./../modals/http-error.js";
+
 const router = Router();
 
-router.get("/", (req, res) => {
-  console.log("GET Request in Places");
-  res.json({ message: "It works!" });
+const DUMMY_PLACES = [
+  {
+    id: "p1",
+    title: "Empire State Building",
+    description: "One of the most famous sky scrapers in the world",
+    imageUrl:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/1/10/Empire_State_Building_%28aerial_view%29.jpg/375px-Empire_State_Building_%28aerial_view%29.jpg",
+    address: "20 W 34th St, New York, NY 10001, United States",
+    creator: "u1",
+    location: { lat: 40.7484405, lng: -73.9878531 },
+  },
+  {
+    id: "p2",
+    title: "Empire State Building",
+    description: "One of the most famous sky scrapers in the world",
+    imageUrl:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/1/10/Empire_State_Building_%28aerial_view%29.jpg/375px-Empire_State_Building_%28aerial_view%29.jpg",
+    address: "20 W 34th St, New York, NY 10001, United States",
+    creator: "u3",
+    location: { lat: 40.7484405, lng: -73.9878531 },
+  },
+];
+
+router.get("/:pid", (req, res) => {
+  const placeId = req.params.pid;
+  const place = DUMMY_PLACES.find((place) => place.id === placeId);
+
+  if (!place) {
+    throw new HttpError("Could not find a place for the provided id.", 404);
+  }
+
+  res.json({ place });
+});
+
+router.get("/user/:uid", (req, res, next) => {
+  const userId = req.params.uid;
+  const place = DUMMY_PLACES.find((place) => place.creator === userId);
+  console.log(place);
+
+  if (!place) {
+    return next(
+      new HttpError("Could not find a place for the provided user id.", 404)
+    );
+  }
+
+  res.json({ place });
 });
 
 export default router;
