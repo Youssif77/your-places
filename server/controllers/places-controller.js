176e77989904d6cd87edb303a42c550172974a26
youssif hany <youssifhany75@gmail.com>
2021-11-21 01:57:43 +0200

    remove dummy-data

--- server/controllers/places-controller.js
+++ server/controllers/places-controller.js
@@ -4,29 +4,6 @@ import HttpError from "../models/http-error.js";
 import getCoordsForAddress from "../util/location.js";
 import Place from "../models/place.js";
 
-let DUMMY_PLACES = [
-  {
-    id: "p1",
-    title: "Empire State Building",
-    description: "One of the most famous sky scrapers in the world",
-    imageUrl:
-      "https://upload.wikimedia.org/wikipedia/commons/thumb/1/10/Empire_State_Building_%28aerial_view%29.jpg/375px-Empire_State_Building_%28aerial_view%29.jpg",
-    address: "20 W 34th St, New York, NY 10001, United States",
-    creator: "u1",
-    location: { lat: 40.7484405, lng: -73.9878531 },
-  },
-  {
-    id: "p2",
-    title: "Empire State Building",
-    description: "One of the most famous sky scrapers in the world",
-    imageUrl:
-      "https://upload.wikimedia.org/wikipedia/commons/thumb/1/10/Empire_State_Building_%28aerial_view%29.jpg/375px-Empire_State_Building_%28aerial_view%29.jpg",
-    address: "20 W 34th St, New York, NY 10001, United States",
-    creator: "u3",
-    location: { lat: 40.7484405, lng: -73.9878531 },
-  },
-];
-
 export const getPlaceById = async (req, res, next) => {
   const placeId = req.params.pid;
 
