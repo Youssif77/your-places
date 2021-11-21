dbabcc7bc7d5a6ee65c24e6a9762759f0a09506b
youssif hany <youssifhany75@gmail.com>
2021-11-21 15:51:25 +0200

    reorder schema

--- server/models/place.js
+++ server/models/place.js
@@ -7,11 +7,11 @@ const placeSchema = new Schema({
   description: { type: String, required: true },
   image: { type: String, required: true },
   address: { type: String, required: true },
-  creator: { type: mongoose.Types.ObjectId, required: true, ref: "User" },
   location: {
     lat: { type: Number, required: true },
     lng: { type: Number, required: true },
   },
+  creator: { type: mongoose.Types.ObjectId, required: true, ref: "User" },
 });
 
 export default mongoose.model("Place", placeSchema);
